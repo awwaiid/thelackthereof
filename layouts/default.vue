92d78fba4773cc6384c909ce1e42045efe853d43
Brock Wilcox <awwaiid@thelackthereof.org>
2022-02-13 18:52:51 -0500

    Update header with cross links

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="m-2">
-
     <div class="m-2 flex items-center">
       <a href="/"><img src="/brock-logo-outline-80x100.png"></a>
-      <div class="ml-4">
-        <h1 class="text-2xl"><a href="/">THE LACK THEREOF</a></h1>
-        <h2 class="text-sm">Various musings of Brock Wilcox (@awwaiid)</h2>
+      <div class="ml-4 flex-grow">
+        <a href="/">
+          <h1 class="lg:text-2xl font-bold">THE LACK THEREOF</h1>
+          <h2 class="text-xs italic">Various musings of Brock Wilcox (@awwaiid)</h2>
+        </a>
+      </div>
+      <div class="grid grid-cols-3">
+        <a class="m-1" href="https://www.github.com/awwaiid"><img width="32px" src="/social-media-icons/github.png" /></a>
+        <a class="m-1" href="https://www.twitter.com/awwaiid/"><img width="32px" src="/social-media-icons/twitter.png" /></a>
+        <a class="m-1" href="https://www.instagram.com/awwaiid/"><img width="32px" src="/social-media-icons/instagram.png" /></a>
+        <a class="m-1" href="https://www.linkedin.com/in/brockwilcox/"><img width="32px" src="/social-media-icons/linkedin.png" /></a>
+        <a class="m-1" href="https://soundcloud.com/awwaiid"><img width="32px" src="/social-media-icons/soundcloud.png" /></a>
+        <a class="m-1" href="https://www.youtube.com/user/awwaiid"><img width="32px" src="/social-media-icons/youtube.png" /></a>
+        <a class="m-1" href="https://www.tiktok.com/@awwaiid"><img width="32px" src="/social-media-icons/tiktok.png" /></a>
+        <a class="m-1" href="mailto:awwaiid@thelackthereof.org"><img width="32px" src="/social-media-icons/email.png" /></a>
       </div>
     </div>
 
